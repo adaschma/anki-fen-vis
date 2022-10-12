386988a05f5659f2b62b4ae8700ba28016acb6fb
Andrew Lamoureux <andrew@vector35.com>
2022-10-12 10:00:05 -0400

    Coordinates, size, colors configurable from add-on manager

--- __init__.py
+++ __init__.py
@@ -1,9 +1,12 @@
 import re
 import sys
+import json
 
 # import bundled chess package
 from . import chess
 from .chess import svg
+
+settings = {}
 
 def qt_breakpoint():
     from PyQt5.QtCore import pyqtRemoveInputHook
@@ -12,12 +15,16 @@ def qt_breakpoint():
     pdb.set_trace()
 
 def fen2svg(m):
+    global config
+
     # m: re.Match
     fen = m.group(1)
     board = chess.Board()
     board.set_fen(fen)
-    svg = chess.svg.board(board, orientation=board.turn, size=320, coordinates=False)
-    return svg
+
+    cmap = {'square light':config['square_light'], 'square dark':config['square_dark']}
+    svg = chess.svg.board(board, orientation=board.turn, size=config['size'], coordinates=config['coordinates'], colors=cmap)
+    return f'<!-- FEN:{fen} -->' + svg
 
 def hook(txt, editor):
     # txt: str
@@ -27,10 +34,30 @@ def hook(txt, editor):
 
 if sys.executable.endswith('anki'):
     print('anki_fen_vis: executing from ANKI gui')
+
+    # read config
+    from aqt import mw
+    config = mw.addonManager.getConfig(__name__)
+    print(f'anki_fen_vis: config {config}')
+
+    # set hook
     from aqt import gui_hooks
     gui_hooks.editor_will_munge_html.append(hook)
+
 elif sys.executable.endswith('python'):
-    print('anki_fen_vis: executing in command-line')
+    print('anki_fen_vis: executing from command-line')
+
+    # read config
+    import os
+    import inspect
+    this_file = inspect.stack()[0][1]
+    this_dir = os.path.dirname(this_file)
+    fpath_json = os.path.join(this_dir, "config.json")
+    print(f'reading: {fpath_json}')
+    with open(fpath_json) as fp:
+        config = json.load(fp)
+    print(f'config: {config}')
+
 else:
     print('anki_fen_vis: executing ???')
 
